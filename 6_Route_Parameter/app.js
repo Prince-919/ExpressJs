5a6f99f9796a7bebb94c85f132881020c6d5e246
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2023-01-29 01:14:52 +0530

    query string and parem adding

--- 6_Route_Parameter/app.js
+++ 6_Route_Parameter/app.js
@@ -27,12 +27,42 @@ const PORT = process.env.PORT || 3000;
 //     res.send(`Travelling From  ${from} To ${to} `)
 // })
 
-app.get('/location/:state.:city', (req, res) => {
-    const {state, city} = req.params;
-    res.send(`Location ${state} . ${city}`);
-})
+// app.get('/location/:state.:city', (req, res) => {
+//     const {state, city} = req.params;
+//     res.send(`Location ${state} . ${city}`);
+// })
 
-app.listen(PORT, () =>{
-    
+
+// Regular Expression Using 
+
+// app.get('/student/:id([0-9]{2})', (req, res) => {
+//     const { id } = req.params;
+//     res.send(`Student Deleted. ${id}`)
+// })
+
+// app.get('/:type(post|article)/:id', (req, res) =>{
+//     console.log(req.params);
+//     res.send('Post or Article')
+// })
+
+// app.param('id', (req, res, next, id) => {
+//     console.log(`Id : ${id}`);
+//     next();
+// })
+// app.get('/user/:id', (req, res) => {
+//     console.log('This is user id path');
+//     res.send('Response OK');
+// })
+
+
+// Query String
+
+app.get('/user', (req, res) => {
+    console.log(req.query);
+    res.send(`Response Ok ${req.query.category}`)
+});
+
+app.listen(PORT, () => {
+
     console.log(`Listening on port http:localhost${PORT}`);
 });
